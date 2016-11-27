e3efde1386c9cde371e548ac9f6cb7cd7a878c84
Jamieson Webking <jwebking@gmail.com>
2016-11-27 10:11:40 -0500

    Add files via upload

---
+++ rando.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  	$.ajaxSetup({cache: false});
+  var quote;
+  var author;
+
+	function getNewQuote() {
+		$.getJSON("//quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(data) {
+			if(data[0].content.length > 120) {
+				getNewQuote();
+			}
+			else{
+				$(".quote").html(data[0].content);
+				var quoteAuthor = " —" + data[0].title;
+				$(".author").html(quoteAuthor);
+			}
+		});
+	}
+  getNewQuote();
+
+    $('#newQuote').on('click', function() {
+    getNewQuote();
+    });
+
+  $('#tweet').on('click', function() {
+   window.open('https://twitter.com/intent/tweet?text=' + $('.quote').text() + " - " + $('.author').text());
+});
+});
