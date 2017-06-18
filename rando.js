1aa523b01d9369b6943101c7c4fa3f6f111a62c4
Jamieson Webking <jwebking@gmail.com>
2017-06-17 22:51:28 -0400

    Update rando.js

--- rando.js
+++ rando.js
@@ -1,27 +1,28 @@
-$(document).ready(function() {
-  	$.ajaxSetup({cache: false});
-  var quote;
-  var author;
+$(document).ready(function () {
+	$.ajaxSetup({ cache: false });
+	var quote;
+	var author;
+
 
 	function getNewQuote() {
-		$.getJSON("//quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(data) {
-			if(data[0].content.length > 120) {
+		$.getJSON("//quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function (data) {
+			if (data[0].content.length > 120) {
 				getNewQuote();
 			}
-			else{
+			else {
 				$(".quote").html(data[0].content);
 				var quoteAuthor = " —" + data[0].title;
 				$(".author").html(quoteAuthor);
 			}
 		});
 	}
-  getNewQuote();
+	getNewQuote();
 
-    $('#newQuote').on('click', function() {
-    getNewQuote();
-    });
+	$('#newQuote').on('click', function () {
+		getNewQuote();
+	});
 
-  $('#tweet').on('click', function() {
-   window.open('https://twitter.com/intent/tweet?text=' + $('.quote').text() + " - " + $('.author').text());
-});
+	$('#tweet').on('click', function () {
+		window.open('https://twitter.com/intent/tweet?text=' + $('.quote').text() + " - " + $('.author').text());
+	});
 });
